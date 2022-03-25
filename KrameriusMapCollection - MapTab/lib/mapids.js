d8982b738f292626c3beb3d9ac827095a33c904e
kvetjo <71756310+kvetjo@users.noreply.github.com>
2022-03-25 12:29:06 +0100

    Add files via upload

---
+++ KrameriusMapCollection - MapTab/lib/mapids.js
@@ -0,0 +1,6 @@
+module.exports = [
+  'uuid:50ef709e-901e-11ec-939e-fa163e4ea95f',
+  'uuid:4d0d8610-3bcc-11ec-a083-fa163e4ea95f',
+  'uuid:1377b9dc-834f-11ec-a2f6-fa163e4ea95f',
+  'uuid:18b3e7cc-48b7-11ec-8259-fa163e4ea95f'
+];
